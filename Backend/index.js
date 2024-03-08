556c7def892a08f039e0e48b20b92f6ce4534323
Sagar Haldar <sagarhaldar987@gmail.com>
2024-03-08 23:27:22 +0530

    Hello

--- Backend/index.js
+++ Backend/index.js
@@ -20,7 +20,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use("/images",express.static(path.join(__dirname,"/images")))
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://blog-app-eosin-one.vercel.app",
     credentials:true
 }))
 // Middleare for Image Upload using Multer
